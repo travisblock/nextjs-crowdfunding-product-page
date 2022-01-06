ba825d3563bcd2e98578f930ddcb07d6db45bfb3
ajid2 <ajidalmajid6@gmail.com>
2022-01-06 20:55:22 +0700

    Modal components (hasn't fixed)
    Signed-off-by: ajid2 <ajidalmajid6@gmail.com>

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,22 @@
+import style from "./style.module.css"
+
+export default function Modal({ title, desc }) {
+    return (
+        <>
+            <div className={style.modalOverlay}></div>
+            <div className={style.modalBox}>
+                <div className={style.modal}>
+                    <div className={style.modalHead}>
+                        <h2 className={style.title}>{ title }</h2>
+                        <p className={style.desc}>{ desc }</p>
+                        <button className={style.close}>
+                            <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.314 0l2.828 2.828L9.9 7.071l4.243 4.243-2.828 2.828L7.07 9.9l-4.243 4.243L0 11.314 4.242 7.07 0 2.828 2.828 0l4.243 4.242L11.314 0z" fill="#000" fill-rule="evenodd" opacity=".4"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
